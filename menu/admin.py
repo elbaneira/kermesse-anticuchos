48a9deba606e82741473dc41fdb2a6f2b3220651
ElbaN <elbaneira@gmail.com>
2026-09-11 12:10:42 -0300

    Corrigiendo registro de Venta en admin.py

--- menu/admin.py
+++ menu/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from .models import Venta
 
-@admin.register(Venta)
 class VentaAdmin(admin.ModelAdmin):
-    list_display = ('id', 'cliente', 'cajero', 'curso_cajero', 'turno', 'cantidad', 'total', 'medio_pago', 'fecha_hora')
+    list_display = ('id', 'cliente', 'medio_pago', 'fecha_hora') # Adapta según tus campos
     list_filter = ('curso_cajero', 'turno', 'medio_pago')
     search_fields = ('cliente', 'cajero__username')
     ordering = ('-fecha_hora',)
+
+# Registro directo al final del archivo
+admin.site.register(Venta, VentaAdmin)
